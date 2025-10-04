57d487e1f8ac4adddc9ffe168e137312cb02b869
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-04 16:36:22 -0300

    Enhancement: Alterando div para main na rota de Login.

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,5 +1,5 @@
 export default function Login() {
     return (
-        <div className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('/bg-fundo.png')]"></div>
+        <main  className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('/bg-fundo.png')]"></main>
     )
 }
