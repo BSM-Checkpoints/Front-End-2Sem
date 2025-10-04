e18cc4cf3f45e1802cd9a0e2f254a70ec06ae920
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-04 17:03:55 -0300

    Feat: Criando o container principal.

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,5 +1,9 @@
 export default function Login() {
     return (
-        <main  className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('/bg-fundo.png')]"></main>
+        <main  className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('/bg-fundo.png')]">
+            <div className="flex w-[900px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-md text-[var(--color-white)">
+                
+            </div>
+        </main>
     )
 }
