4cb3e0de0ddf872530db4f960427d011a9370aee
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-04 20:18:21 -0300

    Enhancement: Melhorando estilização do container de saudação da página de Login e adicionando nova variável de cor para os botões.

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -12,32 +12,37 @@ export default function Login() {
 
     return (
         <main className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('bg-fundo.png')] flex items-center justify-center">
-            <div className="flex w-[900px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-sm text-[var(--color-white)">
-                <div>
-                    <div>
-                        <h1 className="text-4xl font-bold mb-2">Seja bem-vindo!</h1>
-                        <p className="text-sm mb-6">
+            <div className="flex w-4xl h-[500px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-sm text-[var(--color-white)] border-1">
+                <div className="w-1/2">
+                    <div className="text-start ml-6">
+                        <h1 className="text-5xl font-bold mt-12 mb-6">Seja bem-vindo!</h1>
+                        <p className="text-lg">
                             Estamos felizes em ter você aqui novamente!
                         </p>
                     </div>
-                    <div className="flex flex-col gap-1">
-                        <p className="text-sm">Não possui uma conta?</p>
-                        <Link to="/cadastro" className="mt-3 px-10 py-2 rounded-xl bg-[var(--color-blue)] hover:bg-[var(--color-blue-hover)] transition">
+                    <div className="flex flex-col gap-1 ml-6 mt-59">
+                        <p className="text-lg">Não possui uma conta?</p>
+                        <Link to="/cadastro" className="text-center text-lg font-semibold w-[150px] mt-3 py-2 rounded-xl border-1 bg-[var(--buttons-color)] hover:bg-[var(--color-blue-hover)] transition">
                             Criar Conta
                         </Link>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center p-10 w-1/2 border-l border-[var(--border-transparent-02)]">
-                    <h2 className="text-4xl text-center font-semibold mb-6">Login</h2>
+
+                <hr className=" flex self-center border-1 h-[450px] border-[var(--border-transparent-02)]"></hr>
+
+                <div className="flex flex-col justify-center p-10 w-1/2">
+                    <h2 className="text-5xl text-center font-semibold mb-6">Login</h2>
                     <form onSubmit={handleSubmit(aoSubmeter)} className="flex flex-col space-y-4">
 
-                        <input type="text" placeholder="Digite seu nome ou usuário" {...register("nomeUsuario", { required: "O nome usuário é obrigatório!" })}
+                        <label>Nome:</label>
+                        <input type="text" placeholder="Digite seu nome ou usuário" {...register("nomeUsuario", { required: "O nome/usuário é obrigatório!" })}
                             className="px-4 py-2 rounded-xl bg-[var(--bg-transparent-02)] border border-[var(--border-transparent-03)] text-[var(--color-white)]
                             placeholder-[--placeholder-grey-300] focus:outline-none"/>
                             {errors.nomeUsuario && (
                                 <p className="text-[var(--color-red)] text-sm">{errors.nomeUsuario.message}</p>
                             )}
 
+                        <label>E-mail:</label>
                         <input type="email" placeholder="Digite seu e-mail" {...register("email", {
                             required: "O e-mail é obrigatório!",
                             pattern: {
