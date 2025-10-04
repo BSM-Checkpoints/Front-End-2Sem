decfb044bb651b0e043975adaded75b437f2feda
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-04 17:30:56 -0300

    Enhancement: Refatorando código do container de saudação.

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom"
 
 export default function Login() {
     return (
-        <main  className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('/bg-fundo.png')]">
-            <div className="flex w-[900px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-md text-[var(--color-white)">
+    <main  className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('
+    bg-fundo.png')]">
+        <div className="flex w-[900px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-md text-[var(--color-white)">
+            <div>
                 <div>
                     <h1 className="text-4xl font-bold mb-2">Seja bem-vindo!</h1>
                     <p className="text-sm mb-6">
-                    Estamos felizes em ter você aqui novamente!
+                        Estamos felizes em ter você aqui novamente!
                     </p>
                 </div>
                 <div className="flex flex-col gap-1">
@@ -17,6 +19,7 @@ export default function Login() {
                     </Link>
                 </div>
             </div>
-        </main>
-    )
+        </div>
+    </main>
+    );
 }
