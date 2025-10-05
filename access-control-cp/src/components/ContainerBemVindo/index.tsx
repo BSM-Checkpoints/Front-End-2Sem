5be348793fb588bf335f32ce44dbfe2f336f471e
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-05 14:57:35 -0300

    Enhancement: Colocado a responsividade para dispositivos pequenos, médios, largos e extra largos.

--- access-control-cp/src/components/ContainerBemVindo/index.tsx
+++ access-control-cp/src/components/ContainerBemVindo/index.tsx
@@ -3,22 +3,28 @@ import type { ContainerBemVindoType } from "../../types/containerBemVindo";
 
 export default function ContainerBemVindo({titulo, subtitulo, pergunta, rota, mensagemRota}: ContainerBemVindoType) {
     return (
-    <div className="flex flex-col justify-center gap-20 items-start p-12 w-1/2">
-        <div>
-            <h1 className="text-4xl font-bold mb-2">{titulo}</h1>
-            <p className="text-sm mb-6">
-                {subtitulo}
-            </p>
+       <div className="lg:ml-5 lg:w-1/2">
+            <div className="text-center lg:text-start ">
+                <h1 className="text-4xl font-bold my-5 sm:text-5xl md:mt-10 lg:mt-8">
+                    {titulo}
+                </h1>
+                <p className="text-lg px-2 sm:text-2xl">
+                    {subtitulo}
+                </p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-1 mt-10
+                    md:mt-15 lg:items-start lg:mt-63 xl:mt-56 "
+                >
+                <p className="text-lg sm:text-xl">{pergunta}</p>
+                <Link 
+                to={rota} 
+                className="text-center text-xl font-semibold w-[150px]
+                 mt-3 py-2 rounded-xl border-1 bg-[var(--buttons-color)]
+                hover:bg-[var(--color-blue-hover)] transition md:w-[180px]
+                md:py-3 md:text-2xl">
+                    {mensagemRota}
+                </Link>
+            </div>
         </div>
-        <div className="flex flex-col gap-1">
-            <p className="text-sm">{pergunta}</p>
-            <Link
-                to={rota}
-                className="mt-3 px-10 py-2 rounded-xl bg-[var(--color-blue)] hover:bg-[var(--color-blue-hover)] transition"
-            >
-                {mensagemRota}
-            </Link>
-        </div>
-    </div>
     )
 }
