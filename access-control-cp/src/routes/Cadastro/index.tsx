ffcd738c73fb05bd8e88f010a579ba8601044cae
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-04 22:29:59 -0300

    Feat: Criado a estrutura da rota Cadastro, colocado a imagem como background

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,7 +1,11 @@
 export default function Cadastro() {
-    return (
-        <div>
-            
-        </div>
-    )
+return (
+        <main className="
+        flex justify-center items-center 
+        h-screen bg-no-repeat bg-cover bg-center 
+        bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('/bg-fundo.png')]
+      ">
+
+        </main>
+)
 }
