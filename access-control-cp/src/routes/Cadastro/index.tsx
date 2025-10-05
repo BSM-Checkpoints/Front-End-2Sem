3acd83ca8182d23ab62cf712d90e8cdea6d65548
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-04 23:04:56 -0300

    Feat: Criado o container principal

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,11 +1,19 @@
 export default function Cadastro() {
-return (
-        <main className="
+  return (
+    <main
+      className="
         flex justify-center items-center 
         h-screen bg-no-repeat bg-cover bg-center 
         bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('/bg-fundo.png')]
-      ">
-
-        </main>
-)
+      "
+    >
+      <div className="
+      flex w-4xl h-[500px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)]
+      backdrop-blur-sm text-[var(--color-white)] border-1
+      "
+      >
+        
+      </div>
+    </main>
+  );
 }
