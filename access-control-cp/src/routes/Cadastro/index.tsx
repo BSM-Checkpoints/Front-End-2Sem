d65478170c030cb4cceba54dfc9041c491d0a8d1
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-05 20:47:00 -0300

    Enhancement: Colocado o bg da imagem para ser min-h-screen

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -52,7 +52,7 @@ export default function Cadastro() {
     <main
       className="
         flex justify-center items-center 
-        h-screen bg-no-repeat bg-cover bg-center 
+        min-h-screen bg-no-repeat bg-cover bg-center 
         bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('/bg-fundo.png')]
       "
     >
