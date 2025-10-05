c8fb96eb8f0a9e21abd734e79bfd90b77e3769d2
Sofia Siqueira <ssofiasiq@gmail.com>
2025-10-05 11:16:16 -0300

    Enhancement: Adicionada responsividade da página de Login para dispositivos pequenos .

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -11,28 +11,30 @@ export default function Login() {
     }
 
     return (
-        <main className="h-screen bg-no-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('bg-fundo.png')] flex items-center justify-center">
-            <div className="flex w-4xl h-[500px] rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-sm text-[var(--color-white)] border-1">
-                <div className="w-1/2">
-                    <div className="text-start ml-6">
-                        <h1 className="text-5xl font-bold mt-12 mb-6">Seja bem-vindo!</h1>
-                        <p className="text-lg">
+        <main className="w-screen h-screen bg-repeat bg-cover bg-center bg-[linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3)),url('bg-fundo.png')] flex items-center justify-center">
+            <div className="flex w-[90%] flex-col h-fit rounded-2xl shadow-lg bg-[var(--bg-transparent-01)] backdrop-blur-sm text-[var(--color-white)] border-1
+            sm:w-[75%]">
+                <div>
+                    <div>
+                        <h1 className="text-4xl font-bold text-center my-5 sm:text-5xl">Seja bem-vindo!</h1>
+                        <p className="text-lg text-center px-2 sm:text-2xl">
                             Estamos felizes em ter você aqui novamente!
                         </p>
                     </div>
-                    <div className="flex flex-col gap-1 ml-6 mt-59">
-                        <p className="text-lg">Não possui uma conta?</p>
-                        <Link to="/cadastro" className="text-center text-xl font-semibold w-[150px] mt-3 py-2 rounded-xl border-1 bg-[var(--buttons-color)] hover:bg-[var(--color-blue-hover)] transition">
+                    <div className="flex flex-col items-center justify-center gap-1 mt-10">
+                        <p className="text-lg sm:text-xl">Não possui uma conta?</p>
+                        <Link to="/cadastro" className="text-center text-xl font-semibold w-[150px] mt-3 py-2 rounded-xl border-1 bg-[var(--buttons-color)]
+                        hover:bg-[var(--color-blue-hover)] transition">
                             Criar conta
                         </Link>
                     </div>
                 </div>
 
-                <hr className=" flex self-center border-1 h-[450px] border-[var(--border-transparent-02)]"></hr>
+                <hr className="flex self-center border-1 w-[90%] border-[var(--border-transparent-02)] my-8"></hr>
 
-                <div className="flex flex-col w-1/2">
-                    <h2 className="text-5xl text-center font-bold mt-10 mb-7">Login</h2>
-                    <form onSubmit={handleSubmit(aoSubmeter)} className="flex flex-col px-10">
+                <div className="flex flex-col">
+                    <h2 className="text-5xl text-center font-bold mb-7">Login</h2>
+                    <form onSubmit={handleSubmit(aoSubmeter)} className="flex flex-col px-7 sm:px-20">
 
                         <label className="mb-2 text-2xl font-semibold">Nome:</label>
                         <input type="text" placeholder="Digite seu nome ou usuário" {...register("nomeUsuario", { required: "O nome/usuário é obrigatório!"})}
@@ -42,7 +44,7 @@ export default function Login() {
                                 <p className="w-[240px] p-2 rounded-2xl mt-2 text-[var(--color-red)] text-md border-1 border-[var(--color-red)]">{errors.nomeUsuario.message}</p>
                             )}
 
-                        <label className="mb-2 mt-3 text-2xl font-semibold">E-mail:</label>
+                        <label className="mb-2 mt-8 text-2xl font-semibold">E-mail:</label>
                         <input type="email" placeholder="Digite seu e-mail" {...register("email", {
                             required: "O E-mail é obrigatório!",
                             pattern: {
@@ -55,7 +57,7 @@ export default function Login() {
                             <p className="w-[180px] p-2 rounded-2xl mt-2 text-[var(--color-red)] text-md border-1 border-[var(--color-red)]">{errors.email.message}</p>
                         )}
 
-                        <button type="submit" className="self-center text-center text-2xl font-semibold w-[120px] mt-8 py-2 rounded-xl
+                        <button type="submit" className="self-center text-center text-2xl font-semibold w-[120px] my-6 py-2 rounded-xl
                         border-1 bg-[var(--buttons-color)] hover:bg-[var(--color-blue-hover)] transition">
                             Entrar
                         </button>
