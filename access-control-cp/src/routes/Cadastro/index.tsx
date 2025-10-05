78a7010cf9ab3b69edd578038c7989c1d94de878
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-04 23:27:04 -0300

    Feat: Criado a coluna formulário, com os inputs de nome, nomeUsuario e email.

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,6 +1,15 @@
 import ContainerBemVindo from "../../components/ContainerBemVindo";
+import type { LoginType } from "../../types/login";
+import { useForm } from "react-hook-form";
+import type { CadastroType } from "../../types/cadastro";
 
 export default function Cadastro() {
+    const { register, handleSubmit, formState: { errors}, } = useForm<CadastroType>()
+
+    const aoSubmeter = (dados: LoginType) => {
+        console.log(dados)
+    }
+
   return (
     <main
       className="
@@ -21,6 +30,66 @@ export default function Cadastro() {
                 rota="/login"
                 mensagemRota="Entrar"
             />
+        {/* Coluna do Formulário */}
+        <div className="flex flex-col justify-center p-10 w-1/2 border-l border-[var(--border-transparent-02)]">
+          <h2 className="text-4xl text-center font-semibold mb-6">Criar Conta</h2>
+          <form 
+          onSubmit={handleSubmit(aoSubmeter)}
+          className="flex flex-col space-y-4">
+            <input
+              type="text"
+              placeholder="Nome"
+              {...register("nome", {
+                required: "O nome é obrigatório"
+              })}
+              className="
+              px-4 py-2 rounded-xl
+            bg-[var(--bg-transparent-02)] border border-[var(--border-transparent-03)]
+             text-[var(--color-white)] placeholder-[--placeholder-grey-300] focus:outline-none"
+            />
+            {errors.nome && (
+                <p className="text-[var(--color-red)] text-sm">{errors.nome.message}</p>
+            )}
+            <input
+              type="text"
+              placeholder="NomeUsuário"
+              {...register("nomeUsuario", {
+                required: "O nome usuário é obrigatório"
+              })}
+              className="
+              px-4 py-2 rounded-xl
+            bg-[var(--bg-transparent-02)] border border-[var(--border-transparent-03)]
+             text-[var(--color-white)] placeholder-[--placeholder-grey-300] focus:outline-none"
+            />
+            {errors.nomeUsuario && (
+                <p className="text-[var(--color-red)] text-sm">{errors.nomeUsuario.message}</p>
+            )}
+            <input
+              type="email"
+              placeholder="E-mail"
+              {...register("email", {
+                required: "O e-mail é obrigatório",
+                pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Formato de e-mail inválido"
+                }
+              })}
+              className="
+              px-4 py-2 rounded-xl
+            bg-[var(--bg-transparent-02)] border border-[var(--border-transparent-03)]
+             text-[var(--color-white)] placeholder-[--placeholder-grey-300] focus:outline-none"
+            />
+            {errors.email && (
+                <p className="text-[var(--color-red)] text-sm">{errors.email.message}</p>
+              )}
+            <button
+              type="submit"
+              className="mt-4 px-4 py-2 rounded-xl bg-[var(--color-blue)] hover:bg-[var(--color-blue-hover)] transition cursor-pointer"
+            >
+              Criar Conta
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
